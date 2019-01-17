faba262aff6f296bec8d3cf51fbd12398bcdbc37
janibros <erchjani@gmail.com>
2019-01-17 16:53:03 +0530

    ff

--- Demo_Helm/src/main/java/com/test/helm/Demo_Helm/DemowebApplication.java
+++ Demo_Helm/src/main/java/com/test/helm/Demo_Helm/DemowebApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemowebApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[]  args) {
 		SpringApplication.run(DemowebApplication.class, args);
 	}
 }
